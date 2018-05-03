81504491c9e218603064e7b0061b650064ff42f7
rickytorrez <torrez.ricky@ymail.com>
2018-05-03 10:40:13 -0400

    Get;

---
+++ client/src/Components/TaskList.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class TaskList extends Component {
+    state = {
+        tasks: []
+    }
+
+    componentDidMount(){
+        axios.get('http://localhost:8080/tasks')
+            .then(res => {
+                console.log(res);
+                this.setState({ tasks: res.data });
+            })
+    }
+
+    render(){
+        return(
+            <div>
+                <ul>
+                    { this.state.tasks.map(task => <li>{ task.title} { task.description }</li> ) }
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default TaskList;
